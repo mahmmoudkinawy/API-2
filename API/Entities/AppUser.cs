1a84c878da3360741ae349b208474df0d603ccff
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-31 20:35:38 +0200

    added appuser

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
