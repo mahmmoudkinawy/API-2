a24871c19dec6777df388f9f63558d4a5a161da2
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-03 18:49:18 +0200

    Updated User Data

--- API/Program.cs
+++ API/Program.cs
@@ -30,12 +30,6 @@ namespace API
 
             await host.RunAsync();
         }
-
-        //public static void Main(string[] args)
-        //{
-        //    CreateHostBuilder(args).Build().Run();
-        //}
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
